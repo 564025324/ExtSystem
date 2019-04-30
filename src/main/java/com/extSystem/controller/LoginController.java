40577202a4cbe33788538295797d2739de688555
lijwen@ThinkPad <564025324@qq.com>
2019-04-30 16:53:27 +0800

    springboot实现：  1. error.html/welcome.html/login.html完善

--- src/main/java/com/extSystem/controller/LoginController.java
+++ src/main/java/com/extSystem/controller/LoginController.java
@@ -6,24 +6,40 @@ package com.extSystem.controller;
 //import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
 
-	@RequestMapping("/normalLogin")
-	public String forwardLogin() {
-//		Map<String, String[]> reqMap = request.getParameterMap();
-//		System.out.println(reqMap);
+    @RequestMapping("/normalLogin")
+    public String forwardLogin() {
+        System.out.println("登录");
+        return "login";
+    }
 
-		return "login";
-	}
+    @RequestMapping("/submit")
+    public String normalLogin(HttpServletRequest req, HttpServletResponse resp, Model model) {
+        if (req == null) {
+            System.out.println("request is null");
+        }
 
-	@ResponseBody
-	@RequestMapping("/submitLogin")
-	public String normalLogin() {
-		return "login";
-	}
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username != null) {
+            System.out.println(username);
+            System.out.println(password);
+            model.addAttribute("username", username);
+            return "welcome";
+        } else {
+            return "error";
+        }
+
+    }
+
+
 }
